47ac7c652166f9ae7a6bba3f9d08baf39a804274
Jorge1 <agullojorge@gmail.com>
2024-03-31 13:56:29 +0200

    Created viewModel

---
+++ app/src/main/java/com/softyorch/firestorage/ui/xml/upload/UploadXmlViewModel.kt
@@ -0,0 +1,9 @@
+package com.softyorch.firestorage.ui.xml.upload
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class UploadXmlViewModel @Inject constructor(): ViewModel() {
+}
