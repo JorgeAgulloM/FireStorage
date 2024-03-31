1dbbea0f27994554a66e78d82f03f8f645f74a55
Jorge1 <agullojorge@gmail.com>
2024-03-31 14:26:52 +0200

    Implemented binding, buttons and navigation to xml

--- app/src/main/java/com/softyorch/firestorage/MainActivity.kt
+++ app/src/main/java/com/softyorch/firestorage/MainActivity.kt
@@ -2,12 +2,35 @@ package com.softyorch.firestorage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.softyorch.firestorage.databinding.ActivityMainBinding
+import com.softyorch.firestorage.ui.xml.upload.UploadXmlActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.apply {
+            btnNavigateToXml.setOnClickListener {
+                startActivity(UploadXmlActivity.create(this@MainActivity))
+            }
+
+            btnNavigateToCompose.setOnClickListener {
+                
+            }
+        }
     }
 }
