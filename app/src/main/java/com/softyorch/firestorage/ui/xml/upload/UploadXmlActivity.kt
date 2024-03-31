a4c89d72d48b1c2b63870c37b5311727f9c5f981
Jorge1 <agullojorge@gmail.com>
2024-03-31 14:26:29 +0200

    Created self navigation function

--- app/src/main/java/com/softyorch/firestorage/ui/xml/upload/UploadXmlActivity.kt
+++ app/src/main/java/com/softyorch/firestorage/ui/xml/upload/UploadXmlActivity.kt
@@ -1,14 +1,19 @@
 package com.softyorch.firestorage.ui.xml.upload
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
-import com.softyorch.firestorage.R
+import androidx.appcompat.app.AppCompatActivity
 import com.softyorch.firestorage.databinding.ActivityUploadXmlBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class UploadXmlActivity : AppCompatActivity() {
+
+    companion object {
+        fun create(context: Context): Intent = Intent(context, UploadXmlActivity::class.java)
+    }
 
     private lateinit var binding: ActivityUploadXmlBinding
     private val viewModel: UploadXmlViewModel by viewModels()
@@ -27,4 +32,5 @@ class UploadXmlActivity : AppCompatActivity() {
     private fun initListeners() {
 
     }
+
 }
