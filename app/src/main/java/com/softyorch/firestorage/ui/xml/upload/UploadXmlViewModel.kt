1e3fd68a0456008f4d3fff4de90c69c240b49f13
Jorge1 <agullojorge@gmail.com>
2024-04-01 08:28:15 +0200

    Implemented service & added upload function

--- app/src/main/java/com/softyorch/firestorage/ui/xml/upload/UploadXmlViewModel.kt
+++ app/src/main/java/com/softyorch/firestorage/ui/xml/upload/UploadXmlViewModel.kt
@@ -1,9 +1,14 @@
 package com.softyorch.firestorage.ui.xml.upload
 
+import android.net.Uri
 import androidx.lifecycle.ViewModel
+import com.softyorch.firestorage.data.StorageService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class UploadXmlViewModel @Inject constructor(): ViewModel() {
+class UploadXmlViewModel @Inject constructor(private val storageService: StorageService): ViewModel() {
+    fun uploadBasicImage(uri: Uri) {
+        storageService.uploadBasicImage(uri)
+    }
 }
