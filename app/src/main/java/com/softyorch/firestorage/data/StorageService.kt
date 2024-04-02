e551ddd007391f4c7ab12615cf0fa831d5e1fa5b
Jorge1 <agullojorge@gmail.com>
2024-04-02 08:04:04 +0200

    Added example upload/Download image

--- app/src/main/java/com/softyorch/firestorage/data/StorageService.kt
+++ app/src/main/java/com/softyorch/firestorage/data/StorageService.kt
@@ -3,9 +3,16 @@ package com.softyorch.firestorage.data
 import android.net.Uri
 import android.util.Log
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
+import kotlinx.coroutines.CancellableContinuation
+import kotlinx.coroutines.suspendCancellableCoroutine
 import javax.inject.Inject
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
 class StorageService @Inject constructor(private val storage: FirebaseStorage) {
+
+    private val fakeUserId = "a98ha789dgyva087ga09a8fya98f"
 
     fun basicExample() {
         val reference = storage.reference.child("/example/other_user.png")
@@ -15,7 +22,29 @@ class StorageService @Inject constructor(private val storage: FirebaseStorage) {
     }
 
     fun uploadBasicImage(uri: Uri) {
-        val reference = storage.reference.child(uri.lastPathSegment.orEmpty())
+        val reference = storage.reference.child(fakeUserId).child(uri.lastPathSegment.orEmpty())
         reference.putFile(uri)
+    }
+
+    suspend fun uploadAndDownloadImage(uri: Uri): Uri =
+        suspendCancellableCoroutine { cancellableContinuation ->
+            val reference = storage.reference.child("/download/${uri.lastPathSegment}")
+            reference.putFile(uri)
+                .addOnSuccessListener {
+                    downloadImage(it, cancellableContinuation)
+                }
+                .addOnFailureListener {
+                    cancellableContinuation.resumeWithException(it)
+                }
+        }
+
+
+    private fun downloadImage(
+        uploadTask: UploadTask.TaskSnapshot,
+        cancellableContinuation: CancellableContinuation<Uri>
+    ) {
+        uploadTask.storage.downloadUrl
+            .addOnSuccessListener { uri -> cancellableContinuation.resume(uri) }
+            .addOnFailureListener { cancellableContinuation.resumeWithException(it) }
     }
 }
